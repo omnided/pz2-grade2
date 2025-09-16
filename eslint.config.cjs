01a1dca072ce72e7675df8c03aec38504b53912a
omnided <stalkerbrey@gmail.com>
2025-09-16 14:10:51 +0300

    Create eslint.config.cjs

---
+++ eslint.config.cjs
@@ -0,0 +1,29 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+  { ignores: ['**/*.cjs'] }, // ігноруємо конфігураційні файли у CJS
+  js.configs.recommended,    // базові правила JS
+  ...tseslint.configs.recommended, // базові правила TS
+  prettier,                  // відключення конфліктів з Prettier
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-unused-vars': 'warn',
+    },
+  },
+];
+
